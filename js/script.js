8cc3dae0e70d024090ff622da16b391ab3d29f54
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2018-01-14 13:42:39 +0300

    Верстка адаптирована к мобильным устройствам

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	//alert($(window).width()+'\n'+$(window).height())
+
+	$(window).on('load', function(){
+		if ($(window).width() <= '400'){
+			$(".PanelMenu").hide();
+			$(".Content").addClass("ContentScroll");
+			$(".Content").removeClass("Content");
+			return this;
+		}
+	});
+
+	$(window).on('resize', function(){
+		if ($(window).width() <= '400'){
+			$(".PanelMenu").hide();
+			$(".Content").addClass("ContentScroll");
+			$(".Content").removeClass("Content");
+			return this;
+		}else{
+			$(".PanelMenu").show();
+			$(".ContentScroll").addClass("Content");
+			$(".ContentScroll").removeClass("ContentScroll");
+		}
+	});
+
+
+});
