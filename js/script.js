9c19cc6785f8824a9ab07dabeafb3ea201140e3d
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2018-03-10 12:34:57 +0300

    В стиль сайта добавлен параллакс эффект

--- js/script.js
+++ js/script.js
@@ -28,5 +28,11 @@ $(document).ready(function(){
 		}
 	});
 
+	$(window).scroll(function(){
+		var scrolled = $(this).scrollTop();
+		$("#backgroundImage").css({
+			"transform" : "translate(0%, -" + scrolled / 25 + "%)"
+		});
 
+	});
 });
