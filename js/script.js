f8160d589be4d3a59df3f96e61638354f4ccd8b2
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2018-06-29 14:34:13 +0300

    Адаптивная верстка сайта

--- js/script.js
+++ js/script.js
@@ -1,38 +1,10 @@
 $(document).ready(function(){
 
-	//alert($(window).width()+'\n'+$(window).height())
-
-	$(window).on('load', function(){
-		if ($(window).width() <= '400'){
-			$(".PanelMenu").hide();
-			$(".Content").addClass("ContentScroll");
-			$(".Content").removeClass("Content");
-			return this;
-		}
-	});
-
-	$(window).on('resize', function(){
-		if ($(window).width() <= '400'){
-			$(".PanelMenu").hide();
-			$(".Content").addClass("ContentScroll");
-			$(".Content").removeClass("Content");
-			return this;
-		}else if($(window).width() <= '770'){
-			$(".PanelMenu").hide();
-			return this;
-		}else{
-			$(".PanelMenu").show();
-			$(".ContentScroll").addClass("Content");
-			$(".ContentScroll").removeClass("ContentScroll");
-			return this;
-		}
-	});
-
 	$(window).scroll(function(){
 		var scrolled = $(this).scrollTop();
 		$("#backgroundImage").css({
 			"transform" : "translate(0%, -" + scrolled / 25 + "%)"
 		});
-
 	});
+
 });
