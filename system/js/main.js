56adefc7624b81880d6f6f9a22bc287ee2437a9c
Somov Studio <somov.studio@gmail.com>
2024-05-06 16:20:44 +0300

    system - menu and panels

--- system/js/main.js
+++ system/js/main.js
@@ -13,9 +13,9 @@
 	var panelE = $("#PanelFilesE");
 	var panelF = $("#PanelFilesF");
 
-	$("#MenuA").click(function(){
-		panelA.show();
-		menuA.addClass("menu-item-selected");
+	function ResetMenu(){
+		panelA.hide();
+		menuA.removeClass("menu-item-selected");
 		panelB.hide();
 		menuB.removeClass("menu-item-selected");
 		panelC.hide();
@@ -26,20 +26,42 @@
 		menuE.removeClass("menu-item-selected");
 		panelF.hide();
 		menuF.removeClass("menu-item-selected");
+	}
+
+	menuA.click(function(){
+		ResetMenu();
+		panelA.show();
+		menuA.addClass("menu-item-selected");
 	});
 
-	$("#MenuB").click(function(){
-		panelA.hide();
-		menuA.removeClass("menu-item-selected");
+	menuB.click(function(){
+		ResetMenu();
 		panelB.show();
 		menuB.addClass("menu-item-selected");
-		panelC.hide();
-		menuC.removeClass("menu-item-selected");
-		panelD.hide();
-		menuD.removeClass("menu-item-selected");
-		panelE.hide();
-		menuE.removeClass("menu-item-selected");
-		panelF.hide();
-		menuF.removeClass("menu-item-selected");
 	});
+
+	menuC.click(function(){
+		ResetMenu();
+		panelC.show();
+		menuC.addClass("menu-item-selected");
+	});
+
+	menuD.click(function(){
+		ResetMenu();
+		panelD.show();
+		menuD.addClass("menu-item-selected");
+	});
+
+	menuE.click(function(){
+		ResetMenu();
+		panelE.show();
+		menuE.addClass("menu-item-selected");
+	});
+
+	menuF.click(function(){
+		ResetMenu();
+		panelF.show();
+		menuF.addClass("menu-item-selected");
+	});
+
 }());
